4b0c526d64876cf768832a572a900cf3eefb423f
swastik-github <62525040+swastik-github@users.noreply.github.com>
2022-04-14 13:15:09 +0530

    img changes

--- components/Sponsors/index.js
+++ components/Sponsors/index.js
@@ -6,19 +6,23 @@ function Sponsors() {
   return (
     <div className={classes.sponsors_container}>
       <h1>Sponsors</h1>
-      <div className={classes.sponsor_box}>
-        <img src="/sponsors/s1.jpeg" />
+      <div style={{ flexDirection: "column" }} className={classes.sponsor_box}>
+        <img className={classes.main_sponsors} src="/sponsors/s1.jpeg" />
+        <img className={classes.main_sponsors} src="/sponsors/s6.jpg" />
       </div>
 
       <h1>Event Sponsors</h1>
       <div className={classes.sponsor_box}>
         <img src="/sponsors/sheryians.png" />
+        <img src="/sponsors/s8.png" />
         <img src="/sponsors/s3.png" />
       </div>
 
       <h1>Food Partners</h1>
       <div className={classes.sponsor_box}>
         <img src="/sponsors/s2.jpeg" />
+        <img src="/sponsors/s7.png" />
+        <img src="/sponsors/s5.png" />
       </div>
     </div>
   );
